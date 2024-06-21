7d2e0db4eaf7d58039b01ea0bfd93dc1b4d83a7d
Enrique Delgado <kikeead@gmail.com>
2024-06-21 14:37:57 -0300

    Update create-cluster.sh

--- 01_eksctl/create-cluster.sh
+++ 01_eksctl/create-cluster.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-# Variables
-CLUSTER_NAME=eks-mundos-e
-AWS_REGION=us-east-1
-
-# Establecer credenciales de AWS 
-aws sts get-caller-identity >> /dev/null  # Este comando verifica las credenciales de AWS para asegurarse de que estén configuradas correctamente.
-# 1-.(aws sts get-caller-identity) Obtiene información sobre la identidad del usuario o servicio que está realizando la llamada.
-# 2-.(>> /dev/null) Redirige la salida estándar (stdout) al archivo /dev/null, lo que significa que no se mostrará en la consola.
-if [ $? -eq 0 ] # Verifica si el comando anterior se ejecutó correctamente (es decir, si su código de salida es 0, todo esta bien).
+   # Variables
+ CLUSTER_NAME=eks-mundos-e
+ AWS_REGION=us-east-1
+   # Establecer credenciales de AWS 
+   # Este comando verifica las credenciales de AWS para asegurarse de que estén configuradas correctamente.
+ aws sts get-caller-identity >> /dev/null  
+   # 1-.(aws sts get-caller-identity) Obtiene información sobre la identidad del usuario o servicio que está realizando la llamada.
+   # 2-.(>> /dev/null) Redirige la salida estándar (stdout) al archivo /dev/null, lo que significa que no se mostrará en la consola.
+   # Verifica si el comando anterior se ejecutó correctamente (es decir, si su código de salida es 0, todo esta bien).
+if [ $? -eq 0 ] 
 then
   echo "Credenciales testeadas, proceder con la creacion de cluster."
-
-  # Creacion de cluster:  
-  # --ssh-access \ Permite el acceso SSH a los nodos del clúster.
-  # --ssh-public-key final-mundosE \ Indicar el nombre de la key.pem.
-  # --managed \  Crea un clúster administrado por EKS.
-  # --full-ecr-access \ Proporciona acceso completo a Amazon Elastic Container Registry (ECR) para los nodos del clúster.
-  # --zones us-east-1a,us-east-1b,us-east-1c \  Define las zonas de disponibilidad en las que se distribuirán los nodos del clúster
+    # Creacion de cluster:  
+    # --ssh-access \ Permite el acceso SSH a los nodos del clúster.
+    # --ssh-public-key final-mundosE \ Indicar el nombre de la key.pem.
+    # --managed \  Crea un clúster administrado por EKS.
+    # --full-ecr-access \ Proporciona acceso completo a Amazon Elastic Container Registry (ECR) para los nodos del clúster.
+    # --zones us-east-1a,us-east-1b,us-east-1c \  Define las zonas de disponibilidad en las que se distribuirán los nodos del clúster
   eksctl create cluster \
-  --name $CLUSTER_NAME \
-  --region $AWS_REGION \
-  --nodes 3 \
-  --node-type t2.micro \
-  --with-oidc \  
-  --ssh-access \ 
-  --ssh-public-key final-mundosE \ 
-  --managed \  
-  --full-ecr-access \ 
-  --zones us-east-1a,us-east-1b,us-east-1c 
-
+    --name $CLUSTER_NAME \
+    --region $AWS_REGION \
+    --nodes 3 \
+    --node-type t2.micro \
+    --with-oidc \
+    --ssh \
+    --ssh-public-key final-mundosE \
+    --managed \
+    --zones us-east-1a,us-east-1b,us-east-1c
   if [ $? -eq 0 ]
   then
     echo "Cluster Setup Completo con eksctl ."
