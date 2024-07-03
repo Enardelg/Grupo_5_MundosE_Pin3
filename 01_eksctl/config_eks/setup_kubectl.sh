848e6617187ee94eb91cb875c38598f7031bbda9
Enrique Delgado <kikeead@gmail.com>
2024-07-03 20:54:12 -0300

    Update setup_kubectl.sh

--- 01_eksctl/config_eks/setup_kubectl.sh
+++ 01_eksctl/config_eks/setup_kubectl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Configura kubectl para el usuario administrador
-aws eks --region us-east-1 update-kubeconfig --name eks-mundos-e
+AWS_REGION=$1
+CLUSTER_NAME=$2
+
+aws eks --region $AWS_REGION update-kubeconfig --name $CLUSTER_NAME
 kubectl get nodes
