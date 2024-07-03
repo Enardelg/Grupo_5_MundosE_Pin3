5e83a1930d5ba25ea98463e65f8f6a52e5949c16
Enrique Delgado <kikeead@gmail.com>
2024-07-03 20:50:27 -0300

    Update edit_aws_auth.sh

--- 01_eksctl/config_eks/edit_aws_auth.sh
+++ 01_eksctl/config_eks/edit_aws_auth.sh
@@ -2,12 +2,13 @@
 # Recibe IAM_USER_ARN como $1 y KUBE_NAMESPACE como $2
 IAM_USER_ARN=$1
 KUBE_NAMESPACE=$2
+PROFILE_NAME=$3
 
 # Edita el ConfigMap aws-auth
 kubectl get configmap aws-auth -n $KUBE_NAMESPACE -o yaml > aws-auth.yaml
 cat <<EOT >> aws-auth.yaml
     - userarn: $IAM_USER_ARN
-      username: your-iam-user
+      username: $PROFILE_NAME
       groups:
         - system:masters
 EOT
