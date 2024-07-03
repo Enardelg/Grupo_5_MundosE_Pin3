01c8bbd17ae345d97d2aa9e5430a26e959d6f0cb
Enrique Delgado <kikeead@gmail.com>
2024-07-03 16:35:19 -0300

    Create edit_aws_auth.sh

---
+++ 01_eksctl/edit_aws_auth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Recibe IAM_USER_ARN como $1 y KUBE_NAMESPACE como $2
+IAM_USER_ARN=$1
+KUBE_NAMESPACE=$2
+
+# Edita el ConfigMap aws-auth
+kubectl get configmap aws-auth -n $KUBE_NAMESPACE -o yaml > aws-auth.yaml
+cat <<EOT >> aws-auth.yaml
+    - userarn: $IAM_USER_ARN
+      username: your-iam-user
+      groups:
+        - system:masters
+EOT
+kubectl apply -f aws-auth.yaml -n $KUBE_NAMESPACE
