eaa171aadb6950cd876ad73f34d1f127cdbe6f59
Enrique Delgado <kikeead@gmail.com>
2024-07-06 15:35:08 -0300

    Update configmap.sh

--- 01_eksctl/config_eks/configmap.sh
+++ 01_eksctl/config_eks/configmap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-kubectl edit -n kube-system configmap/aws-auth
-
-mapUsers: |
-    - userarn: arn:aws:iam::123456789012:user/user-eks
-      username: user-eks
-      groups:
-        - system:masters
+#1-. Preparar kubectl para comunicarse con el cluster
+aws eks --region us-east-1 update-kubeconfig --name eks-mundos-e
+#2-. Intentar ver contenido con otro usuario
+aws configure --profile <nombre-usuario>
+#3-. Configura kubeconfig para el usuario anteriormente insertado
+aws eks --region us-east-1 update-kubeconfig --name <nombre-cluster> --profile <nombre-usuario>
+#4-. Intenta ver info del cluster
+kubectl get nodes
+#**tendria que figurar este error: 
+#Error from server (Forbidden): nodes is forbidden: User "arn:aws:iam::123456789012:user/new-user" cannot list resource "nodes" in API group "" at the cluster scope
