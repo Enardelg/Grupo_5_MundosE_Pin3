c39561046080d42b0d65b95acac1187774a331ea
Enrique Delgado <kikeead@gmail.com>
2024-07-06 15:33:51 -0300

    Delete 01_eksctl/config_eks/setup_kubectl.sh

--- 01_eksctl/config_eks/setup_kubectl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Configura kubectl para el usuario administrador
-AWS_REGION=$1
-CLUSTER_NAME=$2
-
-aws eks --region $AWS_REGION update-kubeconfig --name $CLUSTER_NAME
-kubectl get nodes
